897151fab555f657bb61c88e5c0a669072bae771
laolution <laolution@gmail.com>
2022-10-14 00:42:16 +0300

    ewq

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,19 +1,49 @@
 // import {users} from "../../data";
 import User from "../user/User";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function Users() {
-    let [users,setUsers] = useState([]);
+    let [users, setUsers] = useState([]);
+    let [user, setUser] = useState({})
 
-    fetch(`https://jsonplaceholder.typicode.com/users`)
-        .then(value=> value.json())
-        .then(item =>{
-            setUsers(item)
+    const lift= (asd)=>{
+        setUser(asd)
+    }
 
-        });
+    // useEffect(() => {
+    //
+    //     fetch(`https://jsonplaceholder.typicode.com/users`)
+    //         .then(value => value.json())
+    //         .then(item => {
+    //             setUsers(item)
+    //
+    //
+    //         });
+    //
+    // }, [])
 
-return (<div>
-        {users.map(user => (<User item={user} key={user.id}/>))}
-</div>
-);
+    useEffect(() => {
+
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+            .then(value => value.json())
+            .then(item => {
+                setUsers(item)
+
+
+            });
+
+    }, [])
+
+
+    return (<div>
+            <hr/>
+            {/*{user && <div>{JSON.stringify(user)}</div>}*/}
+            {/*{user?<div>asdfasd </div>:<div>weweee</div>}*/}
+            {/*{JSON.stringify(user)}*/}
+            <h3>{user?.username}</h3>
+            <hr/>
+
+            {users.map(user => (<User item={user} key={user.id} lift={lift}/>))}
+        </div>
+    );
 }
--- src/components/user/User.js
+++ src/components/user/User.js
@@ -1,10 +1,15 @@
 export default function User(props) {
 
-    let {item:user} = props;
+    let {item: user,lift} = props;
 
     return (<div>
             <h2>{user.id} - {user.name}</h2>
             <p>{user.address.city} <br/> {user.email}</p>
+
+            <button onClick={() => {
+                lift(user)
+            }}>details
+            </button>
         </div>
     );
 };
